2f3721fd13ea1476ea33d18c2b5d554eb7baf4a8
Sakshi Chilkoti <145189160+SakshiChilkoti@users.noreply.github.com>
2024-05-18 11:45:55 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,17 @@
+
+from gtts import gTTS
+
+import os
+
+
+mytext = 'Welcome'
+
+
+language = 'en'
+
+myobj = gTTS(text=mytext, lang=language, slow=False)
+
+
+myobj.save("welcome.mp3")
+
+os.system("start welcome.mp3")
